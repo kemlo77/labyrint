5b81faca2f0bf6565b17b98410ae88e23b6bb4d8
Fredrik Kemling <kemling@gmail.com>
2024-03-14 21:02:23 +0100

    Refactor neighbour count verification in OctagonalGridCreator

--- test/octagonalgridcreator.spec.ts
+++ test/octagonalgridcreator.spec.ts
@@ -5,19 +5,14 @@ import { expect } from 'chai';
 describe('OctagonalGridCreator', () => {
 
     it('Verifying neighbour count', () => {
-        const grid: Grid = OctagonalGridCreator.createGrid(7, 5, 10);
+        const grid: Grid = OctagonalGridCreator.createGrid(4, 3, 10);
 
-        expect(grid.allCells.length).to.equal(33);
-        grid.printNeighboursPerCell();
+        expect(grid.allCells.length).to.equal(18);
 
-        expect(grid['_cellMatrix'][0].map(cell => cell.neighbours.length)).to.deep.equal([3, 4, 5, 2]);
-        expect(grid['_cellMatrix'][1].map(cell => cell.neighbours.length)).to.deep.equal([4, 4, 4, 4]);
-        expect(grid['_cellMatrix'][2].map(cell => cell.neighbours.length)).to.deep.equal([4, 4, 4, 4]);
-        expect(grid['_cellMatrix'][3].map(cell => cell.neighbours.length)).to.deep.equal([3, 3, 3, 3]);
-        expect(grid['_cellMatrix'][4].map(cell => cell.neighbours.length)).to.deep.equal([2, 3, 3, 2]);
-
-
-
+        expect(grid['_cellMatrix'][0].map(cell => cell.neighbours.length)).to.deep.equal([3, 4, 5, 4, 3]);
+        expect(grid['_cellMatrix'][1].map(cell => cell.neighbours.length)).to.deep.equal([5, 4, 8, 4, 5]);
+        expect(grid['_cellMatrix'][2].map(cell => cell.neighbours.length)).to.deep.equal([5, 4, 8, 4, 5]);
+        expect(grid['_cellMatrix'][3].map(cell => cell.neighbours.length)).to.deep.equal([3, 0, 5, 0, 3]);
 
     });
 

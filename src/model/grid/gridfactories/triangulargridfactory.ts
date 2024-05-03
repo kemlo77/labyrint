764b5dd40ad5f277e37c4ff38c17f9417a97d0fb
Fredrik Kemling <kemling@gmail.com>
2024-05-03 21:43:11 +0200

    Refactoring TriangularGridFactory

--- src/model/grid/gridfactories/triangulargridfactory.ts
+++ src/model/grid/gridfactories/triangulargridfactory.ts
@@ -1,6 +1,10 @@
+import { MatrixOperations } from '../../../service/matrixoperations';
 import { Coordinate } from '../../coordinate';
+import { rightUnitVector, upUnitVector } from '../../unitvectors';
+import { Vector } from '../../vector';
 import { Cell } from '../cell/cell';
 import { CellFactory } from '../cell/cellfactory';
+import { CellCreator } from '../cell/celltypealiases';
 import { Grid } from '../grid';
 import { UnframedGridFactory } from './unframedgridfactory';
 
@@ -8,13 +12,12 @@ import { UnframedGridFactory } from './unframedgridfactory';
 export class TriangularGridFactory extends UnframedGridFactory {
 
     createGrid(numberOfColumns: number, numberOfRows: number, cellWidth: number): Grid {
-        const cellMatrix: Cell[][] = this.createCellMatrix(numberOfColumns, numberOfRows, cellWidth);
-        this.establishNeighbourRelationsInMatrix(cellMatrix);
+        const cellGrid: Cell[][] = this.createCellMatrix(numberOfColumns, numberOfRows, cellWidth);
+        this.establishNeighbourRelationsInMatrix(cellGrid);
 
-        const startCell: Cell = cellMatrix[0][0];
-        startCell.visited = true;
-        const endCell: Cell = cellMatrix[numberOfColumns - 1][numberOfRows - 1];
-        const cells: Cell[] = cellMatrix.flat();
+        const startCell: Cell = cellGrid[0][0];
+        const endCell: Cell = cellGrid[cellGrid.length - 1][cellGrid[cellGrid.length - 1].length - 1];
+        const cells: Cell[] = cellGrid.flat();
         return new Grid(cells, startCell, endCell);
     }
 
@@ -22,25 +25,38 @@ export class TriangularGridFactory extends UnframedGridFactory {
     private createCellMatrix(numberOfColumns: number, numberOfRows: number, cellWidth: number): Cell[][] {
         const cellHeight: number = Math.sqrt(3) / 2 * cellWidth;
 
-        const grid: Cell[][] = [];
-        for (let columnIndex: number = 0; columnIndex < numberOfColumns; columnIndex++) {
-            const rowOfCells: Cell[] = [];
-            for (let rowIndex: number = 0; rowIndex < numberOfRows; rowIndex++) {
-                const xCoordinate: number = cellWidth * (1 + columnIndex / 2);
+        const columnStep: Vector = rightUnitVector.scale(cellWidth / 2);
+        const rowStep: Vector = upUnitVector.scale(cellHeight);
+        const flatTopTriangleExtraRowStep: Vector = upUnitVector.scale(cellHeight / 3);
 
+        const createFlatTopTriangle: CellCreator = (center: Coordinate) =>
+            CellFactory.createCell(center, cellWidth, 'equilateral-triangular', 180);
+        const createPointyTopTriangle: CellCreator = (center: Coordinate) =>
+            CellFactory.createCell(center, cellWidth, 'equilateral-triangular', 0);
+
+        const firstCellCenter: Coordinate = new Coordinate(cellWidth, cellHeight);
+
+        const cellRows: Cell[][] = [];
+
+        for (let rowIndex: number = 0; rowIndex < numberOfRows; rowIndex++) {
+            const rowStartCellCenter: Coordinate = firstCellCenter.newRelativeCoordinate(rowStep.scale(rowIndex));
+            const rowOfCells: Cell[] = [];
+            for (let columnIndex: number = 0; columnIndex < numberOfColumns; columnIndex++) {
+                let cellCenter: Coordinate = rowStartCellCenter
+                    .newRelativeCoordinate(columnStep.scale(columnIndex));
                 if (this.cellHasFlatTop(columnIndex, rowIndex)) {
-                    const yCoordinate: number = cellHeight * (4 / 3 + rowIndex);
-                    const center: Coordinate = new Coordinate(xCoordinate, yCoordinate);
-                    rowOfCells.push(CellFactory.createCell(center, cellWidth, 'equilateral-triangular', 180));
+                    cellCenter = cellCenter.newRelativeCoordinate(flatTopTriangleExtraRowStep);
+                    rowOfCells.push(createFlatTopTriangle(cellCenter));
                 } else {
-                    const yCoordinate: number = cellHeight * (1 + rowIndex);
-                    const center: Coordinate = new Coordinate(xCoordinate, yCoordinate);
-                    rowOfCells.push(CellFactory.createCell(center, cellWidth, 'equilateral-triangular', 0));
+                    rowOfCells.push(createPointyTopTriangle(cellCenter));
                 }
             }
-            grid.push(rowOfCells);
+
+            cellRows.push(rowOfCells);
         }
-        return grid;
+
+
+        return cellRows;
     }
 
     private cellHasFlatTop(columnIndex: number, rowIndex: number): boolean {
@@ -50,8 +66,9 @@ export class TriangularGridFactory extends UnframedGridFactory {
     }
 
     private establishNeighbourRelationsInMatrix(grid: Cell[][]): void {
-        this.establishNeighbourRelationsInRows(grid);
-        this.establishSomeNeighbourRelationsInColumns(grid);
+        const translatedGrid: Cell[][] = MatrixOperations.transpose<Cell>(grid);
+        this.establishNeighbourRelationsInRows(translatedGrid);
+        this.establishSomeNeighbourRelationsInColumns(translatedGrid);
     }
 
 

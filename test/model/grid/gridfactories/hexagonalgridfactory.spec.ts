5875dc35e52bad8f80a4858f7e0733a28c689a77
Fredrik Kemling <kemling@gmail.com>
2024-05-11 14:51:56 +0200

    Fixing unit tests

--- test/model/grid/gridfactories/hexagonalgridfactory.spec.ts
+++ test/model/grid/gridfactories/hexagonalgridfactory.spec.ts
@@ -2,29 +2,37 @@ import { expect } from 'chai';
 import { HexagonalGridFactory } from '../../../../src/model/grid/gridfactories/hexagonalgridfactory';
 import { Grid } from '../../../../src/model/grid/grid';
 import { Cell } from '../../../../src/model/grid/cell/cell';
+import { Coordinate } from '../../../../src/model/coordinate';
+import { GridProperties } from '../../../../src/model/grid/gridfactories/gridproperties';
 
 describe('HexagonalGridFactory', () => {
 
+    const insertionPoint: Coordinate = new Coordinate(0, 0);
+    const hexagonalgridfactory: HexagonalGridFactory = new HexagonalGridFactory();
+    const gridProperties: GridProperties = new GridProperties(insertionPoint, 5, 5, 10);
+
+
+
     it('verifying neighbour count', () => {
-        const hexagonalgridfactory: HexagonalGridFactory = new HexagonalGridFactory();
-        const cellMatrix: Cell[][] = hexagonalgridfactory['createCellMatrix'](5, 5, 10);
+
+        const cellMatrix: Cell[][] = hexagonalgridfactory['createCellMatrix'](gridProperties);
         hexagonalgridfactory['establishNeighbourRelationsInGrid'](cellMatrix);
 
-        expect(cellMatrix[0].map(cell => cell.neighbours.length)).to.deep.equal([2, 4, 4, 4, 3]);
-        expect(cellMatrix[1].map(cell => cell.neighbours.length)).to.deep.equal([5, 6, 6, 6, 3]);
-        expect(cellMatrix[2].map(cell => cell.neighbours.length)).to.deep.equal([3, 6, 6, 6, 5]);
-        expect(cellMatrix[3].map(cell => cell.neighbours.length)).to.deep.equal([5, 6, 6, 6, 3]);
-        expect(cellMatrix[4].map(cell => cell.neighbours.length)).to.deep.equal([2, 4, 4, 4, 3]);
+        expect(cellMatrix[0].map(cell => cell.neighbours.length)).to.deep.equal([3, 4, 4, 4, 3]);
+        expect(cellMatrix[1].map(cell => cell.neighbours.length)).to.deep.equal([3, 6, 6, 6, 6, 3]);
+        expect(cellMatrix[2].map(cell => cell.neighbours.length)).to.deep.equal([5, 6, 6, 6, 5]);
+        expect(cellMatrix[3].map(cell => cell.neighbours.length)).to.deep.equal([3, 6, 6, 6, 6, 3]);
+        expect(cellMatrix[4].map(cell => cell.neighbours.length)).to.deep.equal([3, 4, 4, 4, 3]);
     });
 
     it('numberOfVisitedCells', () => {
-        const rectGrid: Grid = new HexagonalGridFactory().createGrid(5, 5, 10);
+        const rectGrid: Grid = hexagonalgridfactory.createGrid(gridProperties);
         expect(rectGrid.numberOfVisitedCells).to.equal(1);
     });
 
     it('totalNumberOfCells', () => {
-        const rectGrid: Grid = new HexagonalGridFactory().createGrid(5, 5, 10);
-        expect(rectGrid.totalNumberOfCells).to.equal(25);
+        const rectGrid: Grid = hexagonalgridfactory.createGrid(gridProperties);
+        expect(rectGrid.totalNumberOfCells).to.equal(27);
     });
 
 
--- test/model/grid/gridfactories/triangulargridfactory.spec.ts
+++ test/model/grid/gridfactories/triangulargridfactory.spec.ts
@@ -1,27 +1,30 @@
 import { expect } from 'chai';
 import { TriangularGridFactory } from '../../../../src/model/grid/gridfactories/triangulargridfactory';
 import { Cell } from '../../../../src/model/grid/cell/cell';
-import { Grid } from '../../../../src/model/grid/grid';
+import { Coordinate } from '../../../../src/model/coordinate';
+import { GridProperties } from '../../../../src/model/grid/gridfactories/gridproperties';
+
 
 describe('TriangularGridFactory', () => {
 
+    const insertionPoint: Coordinate = new Coordinate(0, 0);
+    const triangularGridFactory: TriangularGridFactory = new TriangularGridFactory();
+
     it('verifying number of neighbours', () => {
-        const triangularGridFactory: TriangularGridFactory = new TriangularGridFactory();
-        const cellMatrix: Cell[][] = triangularGridFactory['createCellMatrix'](3, 3, 10);
+        const gridProperties: GridProperties = new GridProperties(insertionPoint, 3, 3, 10);
+        const cellMatrix: Cell[][] = triangularGridFactory['createCellMatrix'](gridProperties);
         triangularGridFactory['establishNeighbourRelationsInMatrix'](cellMatrix);
         const middleCell: Cell = cellMatrix[1][1];
         expect(middleCell.neighbours.length).to.equal(3);
     });
 
     it('verifying neighbour count', () => {
-        const triangularGridFactory: TriangularGridFactory = new TriangularGridFactory();
-        const cellMatrix: Cell[][] = triangularGridFactory['createCellMatrix'](4, 4, 10);
+        const gridProperties: GridProperties = new GridProperties(insertionPoint, 3, 2, 10);
+        const cellMatrix: Cell[][] = triangularGridFactory['createCellMatrix'](gridProperties);
         triangularGridFactory['establishNeighbourRelationsInMatrix'](cellMatrix);
-        expect(cellMatrix[0].map(cell => cell.neighbours.length)).to.deep.equal([2, 2, 2, 2]);
-        expect(cellMatrix[1].map(cell => cell.neighbours.length)).to.deep.equal([2, 3, 3, 2]);
-        expect(cellMatrix[2].map(cell => cell.neighbours.length)).to.deep.equal([3, 3, 3, 3]);
-        expect(cellMatrix[3].map(cell => cell.neighbours.length)).to.deep.equal([1, 2, 2, 1]);
-
+        expect(cellMatrix[0].map(cell => cell.neighbours.length)).to.deep.equal([2, 2, 3, 2, 2]);
+        expect(cellMatrix[1].map(cell => cell.neighbours.length)).to.deep.equal([2, 3, 3, 3, 2]);
+        expect(cellMatrix[2].map(cell => cell.neighbours.length)).to.deep.equal([1, 3, 2, 3, 1]);
     });
 
 });
